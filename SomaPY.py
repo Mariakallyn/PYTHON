344192ed4f2503139af6232504164424857275ea
Maria <maria.silva5160@etec.sp.gov.br>
2024-08-22 22:28:50 -0300

    branch com o código da aula do dia Thu, Aug 22, 2024 10:28:50 PM

---
+++ SomaPY.py
@@ -0,0 +1,9 @@
+#definir variaveis
+numero1 = 5
+numero2 = 3
+
+#processamento de dados
+resultados = numero1 + numero2
+
+#saida de dados
+print("A soma de ", numero1, "e", numero2, "é", resultados)
